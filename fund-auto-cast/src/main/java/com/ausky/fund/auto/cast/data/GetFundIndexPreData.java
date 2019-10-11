a2fce44f4bfecd83b9c84de5f72177029c0424cb
ausky <aohaisong1990516@126.com>
2019-10-11 23:24:43 +0800

    添加基金定投测试

---
+++ fund-auto-cast/src/main/java/com/ausky/fund/auto/cast/data/GetFundIndexPreData.java
@@ -0,0 +1,8 @@
+package com.ausky.fund.auto.cast.data;
+
+/**
+ * 获取基金指数的历史行情
+ */
+public class GetFundIndexPreData {
+
+}
---
+++ fund-auto-cast/src/main/java/com/ausky/fund/auto/cast/trade/FundTradeStrategy.java
@@ -0,0 +1,7 @@
+package com.ausky.fund.auto.cast.trade;
+
+/**
+ * 基金定投交易策略
+ */
+public class FundTradeStrategy {
+}
